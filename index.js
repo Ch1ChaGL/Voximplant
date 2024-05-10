661c267ea5c281766f0b2d9d5d955aa82cac53e7
Danil Markov <markov.danil.04@mail.ru>
2024-05-10 17:33:56 +0300

    1

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const RULE_ID = "1231325";
 
 // Создание CSV файла с контактами
 const csvContacts = `phone;name;
-699119939;Sofia`;
-
-const formData = new FormData();
-formData.append("file_content", csvContacts);
+79522033346;Sofia;`;
 
 const apiUrl =
   API_URL +
@@ -23,9 +20,9 @@ const apiUrl =
   API_KEY +
   "&rule_id=" +
   RULE_ID +
-  "&max_simultaneous=10" +
+  "&max_simultaneous=1" +
   "&num_attempts=2" +
-  "&interval_seconds=60" +
+  "&interval_seconds=15" +
   "&priority=1" +
   "&name=CallList";
 
@@ -33,7 +30,7 @@ btn.onclick = () => {
   // Отправка запроса к API Voximplant для создания списка для обзвона
   fetch(apiUrl, {
     method: "POST",
-    body: formData,
+    body: csvContacts
   })
     .then((response) => response.json())
     .then((data) => {
